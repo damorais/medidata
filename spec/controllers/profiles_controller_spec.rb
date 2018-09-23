80de4ba5e9369e87617f4e794fa6e1f63b95ce9c
Daniel Morais <damorais@gmail.com>
2018-09-23 16:29:17 -0300

    Inclusão de testes unitários no cadastro

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -2,4 +2,38 @@ require 'rails_helper'
 
 RSpec.describe ProfilesController, type: :controller do
 
+    it "Should return the Create your Profile page" do
+        get :new
+        assert_response :success
+    end
+
+    it "Should create a new profile" do
+        expect {
+            post :create, :params => { :profile  => { 
+                email: "joao@example.org",
+                firstname: "João",
+                lastname: "Silva",
+                birthdate: "2000-12-15",
+                sex: "Male",
+                gender: "Male" } 
+            }
+        }.to change(Profile, :count)
+
+        expect(response).to have_http_status(:success)
+    end
+
+    it "Should not allow to create a new profile without required fields" do 
+        expect {
+            post :create, :params => { :profile  => { 
+                                        email: "",
+                                        firstname: "",
+                                        lastname: "",
+                                        birthdate: "",
+                                        sex: "",
+                                        gender: "" } 
+            }
+        }.to_not change(Profile, :count)
+
+        expect(response).to render_template(:new)
+    end
 end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    it "Is valid with valid attributes" do
+      profile = Profile.new(email: "joao_silva@example.org", 
+                            firstname: "João", 
+                            lastname: "Silva", 
+                            birthdate: Date.new(2000, 5, 5))
+
+      expect(profile).to be_valid
+    end
+
+    it "Is not valid without an email" do
+      profile = Profile.new(email: nil)
+      expect(profile).to_not be_valid
+    end
+    
+    it "Is not valid if the email is not unique" do
+      Profile.create(email: "joao_silva@example.org", 
+                     firstname: "João", 
+                     lastname: "Silva", 
+                     birthdate: Date.new(2000, 5, 5))
+      profile = Profile.new(email: "joao_silva@example.org", 
+                            firstname: "João", 
+                            lastname: "Silva", 
+                            birthdate: Date.new(2000, 5, 5))
+      expect(profile).to_not be_valid
+
+    end
+
+    it "Is not valid without a firstname" do
+      profile = Profile.new(firstname: nil)
+      expect(profile).to_not be_valid
+    end
+
+    it "Is not valid without a lastname" do
+      profile = Profile.new(lastname: nil)
+      expect(profile).to_not be_valid
+    end
+
+    it "Is not valid without a birthdate" do 
+      profile = Profile.new(birthdate: nil)
+      expect(profile).to_not be_valid
+    end
+  end
+
 end
