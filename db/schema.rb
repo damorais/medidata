701f203be9a68c2d8879d758530c3e4e9fe4e246
Daniel Morais <damorais@gmail.com>
2018-10-04 16:40:16 -0300

    Ajustes em configurações

--- db/schema.rb
+++ db/schema.rb
@@ -26,10 +26,4 @@ ActiveRecord::Schema.define(version: 2018_09_06_191447) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
