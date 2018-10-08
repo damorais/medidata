74147ef5474e6241ae9eed0d525e5319a9ce2fa0
diegotussi <42522857+diegotussi@users.noreply.github.com>
2018-10-08 01:24:52 -0300

    Add files via upload

---
+++ db/migrate/20181008011701_create_contacts.rb
@@ -0,0 +1,12 @@
+class CreateContacts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :contacts do |t|
+      t.string :contactemail
+      t.string :contactname
+      t.string :contactphone
+      t.string :contactmobile
+
+      t.timestamps
+    end
+  end
+end
