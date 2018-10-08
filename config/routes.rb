cb4532ced89dac2845d7b5a6db712f96f6ecb76b
diegotussi <42522857+diegotussi@users.noreply.github.com>
2018-10-08 01:23:56 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :profiles, param: :email, email:  /[^\/]+/ 
+  resources :profiles, param: :email, email:  /[^\/]+/ do
                         # only: [:index, :new, :create, :edit, :update]
+	resources :contacts 
+  
+  end
   root 'welcome#index'
 
 end
