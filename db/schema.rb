3153cfbecc0d8cf0c68aece3a516cfb33ea0c39f
diegotussi <42522857+diegotussi@users.noreply.github.com>
2018-10-08 01:31:06 -0300

    Add files via upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_06_191447) do
+ActiveRecord::Schema.define(version: 2018_10_08_011701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "contactemail"
+    t.string "contactname"
+    t.string "contactphone"
+    t.string "contactmobile"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.string "email"
