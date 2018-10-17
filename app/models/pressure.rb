49ef04e1d743f4ca6b26eacb7328fa4dbf8bdfdc
William Maruyama <wtmaruyama@gmail.com>
2018-10-17 12:26:19 -0300

    Validacao dos campos.

--- app/models/pressure.rb
+++ app/models/pressure.rb
@@ -1,3 +1,7 @@
 class Pressure < ApplicationRecord
   belongs_to :profile
+
+  validates :sis, presence: { message: "O valor da pressão sistólica é obrigatório." }
+  validates :dia, presence: { message: "O valor da pressão diastólica é obrigatório." }
+  validates :data, presence: { message: "A data da medição é obrigatória." }
 end
