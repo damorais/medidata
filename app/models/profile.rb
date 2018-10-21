bf16b6d8e1713d869d2ef2921c5283b6d0c88d51
diegotussi <42522857+diegotussi@users.noreply.github.com>
2018-10-21 03:21:31 -0300

    Update profile.rb

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,7 @@ class Profile < ApplicationRecord
   has_many :weights
   has_many :heights
   has_many :contacts
+  has_many :allergies
   
   validates :email, uniqueness: { case_sensitive: false,
                                   message: "Já existe um perfil com este e-mail" },
