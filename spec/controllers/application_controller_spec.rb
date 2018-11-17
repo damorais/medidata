1c9fcc8782498b96d1ce9c9752c09e6c30248352
Daniel Morais <damorais@gmail.com>
2018-11-16 23:53:49 -0200

    Autenticação: Redirecionamento após login

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,4 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
+  it 'should redirect to profile path after login' do
+    @user = FactoryBot.create :user, email: 'joao@example.org'
+
+    target_uri = @controller.after_sign_in_path_for(@user)
+
+    expect(target_uri).to eq(profile_path(email: @user.email))
+  end
+
 end
