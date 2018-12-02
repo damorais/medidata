23128f7220cf55935e785f324700db7dd2f425c7
diegotussi <42522857+diegotussi@users.noreply.github.com>
2018-12-01 23:57:44 -0200

    Update glucose_measures.rb

--- spec/factories/glucose_measures.rb
+++ spec/factories/glucose_measures.rb
@@ -3,7 +3,7 @@ require 'date'
 FactoryBot.define do
   factory :glucose_measure do
     value { 1.5 }
-    date { "2018-10-26 17:44:36" }
+    date { "2018-10-26 17:44:36".to_date }
     fasting { false }
     profile { nil }
   end
