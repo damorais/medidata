775319d153bbdfd6a17d7c87c0ab6030ebe21303
Vinicius Eustaquio Cortez <viniciusecortez@outlook.com>
2019-06-11 17:44:40 -0300

    Criado view e controller

---
+++ app/controllers/historic_relatives_controller.rb
@@ -0,0 +1,13 @@
+class HistoricRelativesController < ApplicationController
+
+    def index
+    end
+    def create
+    end
+    def new
+    end
+    def edit
+        
+    end
+
+end
---
+++ spec/controllers/historic_relatives_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe HistoricRelativesController, type: :controller do
+
+end
