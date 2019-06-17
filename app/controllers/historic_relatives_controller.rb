edf09ddfcf8fb434d49c2d7cc473ed122285aec5
Vinicius Eustaquio Cortez <viniciusecortez@outlook.com>
2019-06-17 15:34:39 -0300

    Correção parcial do controller

--- app/controllers/historic_relatives_controller.rb
+++ app/controllers/historic_relatives_controller.rb
@@ -1,5 +1,8 @@
 class HistoricRelativesController < ApplicationController
-
+    layout 'internal'
+    before_action :authenticate_user!
+  before_action :block_crossprofile_access
+  before_action :recover_profile
     def index
     end
     def create
