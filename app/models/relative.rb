bab8806e62779ea9c1ed7e6633a222d02791a0df
Jefferson <jeffersonss999@gmail.com>
2019-06-27 17:44:56 +0000

    Correção do Model de relatives

--- app/models/relative.rb
+++ app/models/relative.rb
@@ -1,13 +1,8 @@
 class Relative < ApplicationRecord
   belongs_to :profile
 
-  after_initialize :set_defaults
-
   validates :name, presence: { message: "The value must be provided" }
   validates :description, presence: { message: "The description must be provided" } 
   validates :kinship, presence: { message: "A kinship must be informed" }
   
-  def set_defaults
-    self.start = Time.now unless self.start
-  end
 end
